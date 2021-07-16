494310f7b4b9d5c76075fee0065dbf99f9805360
VStaykov <v.staykov@razorlabs.com>
2021-07-16 17:42:35 +0300

    organizing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 const Web3 = require('web3');
-const abi = require('./token.json');
-const gravityContractAbi = require('./interface.json');
+const ERC20TokenAbi = require('./solidity/contract_interfaces/ERC20_token.json');
+const gravityContractAbi = require('./solidity/contract_interfaces/gravity.json');
 const bech32 = require('@cosmjs/encoding');
 
 const ERC20ContractAddress = '0x28ea52f3ee46cac5a72f72e8b3a387c0291d586d';
 const cosmosAddress = 'cudos1gg3zzvqsxcqjn95jy5frz969y4thrygq7z8mua';
-const ethPrivateKey = '45c517f199dccb2caece0b802c601ad7407bb658539b89ef80b0fac949c8c051';
 const contractAddress = '0xb114942d4abf8E907015F0a03e421268A0919cca';
+
 const amount = 23561;
 
 
@@ -36,7 +35,7 @@ function App() {
       });
 
 
-      var erc20Instance = new window.web3.eth.Contract(abi,ERC20ContractAddress);
+      var erc20Instance = new window.web3.eth.Contract(ERC20TokenAbi,ERC20ContractAddress);
 
       erc20Instance.methods.approve(contractAddress, amount)
           .send({from: account, gas: 2000000}, 
