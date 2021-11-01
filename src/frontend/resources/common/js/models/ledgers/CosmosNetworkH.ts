d8746368e9d7ef2e356ac7d4ad0ad494f126227f
Kamen Stoykov <k.stoykov@razorlabs.com>
2021-11-01 15:38:39 +0000

    toString conversion

--- src/frontend/resources/common/js/models/ledgers/CosmosNetworkH.ts
+++ src/frontend/resources/common/js/models/ledgers/CosmosNetworkH.ts
@@ -1,8 +1,12 @@
+import BigNumber from 'bignumber.js';
+
 export default class CosmosNetworkH {
+
     static CURRENCY_DISPLAY_NAME = 'CUDOS';
     static CURRENCY_DENOM = 'acudos';
     static CURRENCY_DECIMALS = 18;
     static CURRENCY_COINGECKO_ID = 'cudos';
+    static CURRENCY_1_CUDO = new BigNumber(`1${'0'.repeat(CosmosNetworkH.CURRENCY_DECIMALS)}`);
 
     static LEDGER_COIN_TYPE = 118;
     static BECH32_PREFIX_ACC_ADDR = 'cudos';
@@ -12,4 +16,5 @@ export default class CosmosNetworkH {
     static BECH32_PREFIX_CONS_ADDR = 'cudosvalcons';
     static BECH32_PREFIX_CONS_PUB = 'cudosvalconspub';
     static BECH32_ACC_ADDR_LENGTH = 44;
+
 }
