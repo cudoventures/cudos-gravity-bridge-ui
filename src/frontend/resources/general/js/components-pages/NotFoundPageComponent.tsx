2e9a79a7e22772e33b9cc6074e33a75ec7e1106d
sabinemm <sabine.mica@gmail.com>
2024-05-24 00:47:02 +0100

    style: view cleanup, input cleanup

--- src/frontend/resources/general/js/components-pages/NotFoundPageComponent.tsx
+++ src/frontend/resources/general/js/components-pages/NotFoundPageComponent.tsx
@@ -7,8 +7,8 @@ import ContextPageComponent, {
 } from "./common/ContextPageComponent";
 
 import "./../../css/components-pages/page-not-found-component.css";
-import LayoutBlock from "../../../common/js/components-inc/LayoutBlock";
 import PageComponent from "../../../common/js/components-pages/PageComponent";
+import Input from "../../../common/js/components-inc/Input";
 import { inject, observer } from "mobx-react";
 
 interface Props extends ContextPageComponentProps {}
@@ -16,18 +16,8 @@ interface Props extends ContextPageComponentProps {}
 export default class PageNotFoundComponent extends ContextPageComponent<Props> {
   static layout() {
     const MobXComponent = inject("appStore")(observer(PageNotFoundComponent));
+    /*     @ts-ignore */
     PageComponent.layout(<MobXComponent />);
-  }
-
-  anchorEl: any;
-  checkbox: 0 | 1;
-  datepicker: number;
-
-  constructor(props: Props) {
-    super(props);
-    this.anchorEl = null;
-    this.checkbox = 1;
-    this.datepicker = Date.now();
   }
 
   getPageLayoutComponentCssClassName() {
@@ -36,14 +26,12 @@ export default class PageNotFoundComponent extends ContextPageComponent<Props> {
 
   renderContent() {
     return (
-      <div
-        style={{ width: "1000px", height: "500px", margin: "auto" }}
-        className={"FlexSingleCenter"}
-      >
-        <LayoutBlock>
+      <div className='PageNotFoundContent'>
+        <div className='PageNotFoundContainer'>
           <h1>404</h1>
+          <Input aria-label='Amount input' placeholder='0' type='number' />
           <p>Oops! Something went wrong. Please try again!</p>
-        </LayoutBlock>
+        </div>
       </div>
     );
   }
